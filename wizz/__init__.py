abeda1a703e3a1989ded67d79c542766b0956da1
Adegoke Bright <brightaverix@gmail.com>
2021-07-01 16:40:22 +0100

    .

--- wizz/__init__.py
+++ wizz/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from config import config
 from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -9,9 +10,10 @@ migrate = Migrate()
 mail = Mail()
 
 
-def create_app(config_file='config.py'):
+def create_app(config_name):
     app = Flask(__name__)
-    app.config.from_pyfile(config_file)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
 
     db.init_app(app)
     mail.init_app(app)
--- wizz/config.py
+++ wizz/config.py
@@ -1,15 +1,37 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
-UPLOAD_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.doc', ".pdf"]
-SQLALCHEMY_DATABASE_URI = 'sqlite:///polywizz.db'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-MAIL_SERVER = 'smtp.googlemail.com'
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-INSTAGRAM_MAIL_SUBJECT_PREFIX = 'From polywizz'
-INSTAGRAM_MAIL_SENDER = 'Polywizz Admin'
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOAD_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.doc', ".pdf"]
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///polywizz.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    INSTAGRAM_MAIL_SUBJECT_PREFIX = 'From polywizz'
+    INSTAGRAM_MAIL_SENDER = 'Polywizz Admin'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'default': DevelopmentConfig
+}
+
+
+
+
