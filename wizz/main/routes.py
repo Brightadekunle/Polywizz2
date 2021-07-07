f62a34fcd584790e448b2a2fedde69f9990b4334
Adegoke Bright <brightaverix@gmail.com>
2021-07-07 16:21:03 +0100

    .

--- wizz/main/routes.py
+++ wizz/main/routes.py
@@ -38,7 +38,7 @@ def create(image):
 @main.route('/client_create/<image>', methods=['GET', 'POST'])
 def clientCreate(image):
     imagePdf = NewDocument.query.filter_by(name=image).first()
-    return render_template("create.html", imagePdf=imagePdf.image_file)
+    return render_template("create.html", imagePdf=imagePdf.name)
 
 
 @main.route('/', methods=['GET', 'POST'])
